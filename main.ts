75751aa44d76929d041551320f6690bc6e33b091
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-08-10 13:10:29 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,14 +7,27 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     Ship.turn(Direction.Right, 45)
 })
+let Xs = 0
 let Ship: game.LedSprite = null
 let Speed = 0
 Ship = game.createSprite(2, 2)
 basic.forever(function () {
     if (0 < Speed) {
         Ship.move(1)
-        basic.pause(100)
-        Ship.ifOnEdgeBounce()
+        Xs = 5 - Speed
+        basic.pause(500 / Xs)
+        if (Ship.isTouchingEdge()) {
+            if (0 == Ship.get(LedSpriteProperty.X)) {
+                Ship.set(LedSpriteProperty.X, 4)
+            } else if (4 == Ship.get(LedSpriteProperty.X)) {
+                Ship.set(LedSpriteProperty.X, 0)
+            }
+            if (0 == Ship.get(LedSpriteProperty.Y)) {
+                Ship.set(LedSpriteProperty.Y, 4)
+            } else if (4 == Ship.get(LedSpriteProperty.Y)) {
+                Ship.set(LedSpriteProperty.Y, 0)
+            }
+        }
         Speed += -0.25
     }
 })
